ea146c1168b7a8a6ffa07a76b1feef387260552d
adityasharma1188 <sharmajii1104@gmail.com>
2025-09-21 21:14:24 +0530

    Delete promise5.js

--- promise5.js
+++
@@ -1,7 +0,0 @@
-//  Write a promise that takes a string and resolves with its uppercase version
-let myPromise = new Promise((resolve) => {
-  resolve("helloworld".toUpperCase());
-});
-myPromise.then((result) => {
-  console.log(result);
-});
